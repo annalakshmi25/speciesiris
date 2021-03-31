46c1b02ef7842f41da7445a94325ba4b18dc4a17
annalakshmi25 <annalakshmiv25@gmail.com>
2021-03-31 13:29:12 +0530

    edited

--- iris/views.py
+++ iris/views.py
@@ -14,6 +14,7 @@ def predict(request):
     exp3 = float(request.GET['exp3'])
     exp4 = float(request.GET['exp4'])
     rawdata = staticfiles_storage.path('Iris.csv')
+    
     dataset = pd.read_csv(rawdata)
     X = dataset.iloc[:,1:5].values
     y = dataset.iloc[:,5].values
